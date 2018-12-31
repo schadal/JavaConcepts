50dabe24fdab429d49bf0ee2d27f9815feb97037
sudhe <sudhe@DESKTOP-BNLBJL8.fios-router.home>
2018-12-31 08:16:01 -0500

    added new testcases

--- TddJava/src/com/test/cases/ValidateISBN.java
+++ TddJava/src/com/test/cases/ValidateISBN.java
@@ -2,32 +2,48 @@ package com.test.cases;
 
 public class ValidateISBN {
 
-	public void ValidateISBN() {}
-	
+		
+	private static final int SHORT_ISBN_MULTIPLIER = 11;
+	private static final int SHORT_ISBN_NUMBER = 10;
+
+	/**
+	 * checkISBN
+	 * @param isbnNo
+	 * @return Boolean
+	 * @author sudhe
+	 * @throws NumberFormatException
+	 */
 	public Boolean checkISBN(String isbnNo) {
 
-		if(isbnNo.length() != 10) throw new NumberFormatException("ISBN Numbers must be 10 digits in length.");
+		if(isbnNo.length() != SHORT_ISBN_NUMBER) throw new NumberFormatException("ISBN Numbers must be 10 digits in length.");
 		
+		return isValidShortISBN(isbnNo);
+	}
+
+	/**
+	 * isValid10DigitISBNCode
+	 * @param isbnNo
+	 * @return Boolean
+	 * @author sudheer
+	 * @throws NumberFormatException
+	 */
+	private Boolean isValidShortISBN(String isbnNo) {
 		int total = 0;
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<SHORT_ISBN_NUMBER; i++) {
 			
 			if(!Character.isDigit(isbnNo.charAt(i))) {
 				if(i==9 && isbnNo.charAt(i) == 'X') {
-					total += 10;
+					total += SHORT_ISBN_NUMBER;
 				}else {
 					throw new NumberFormatException("Character is not a numeric digit.");	
 				}
 				
 			}else {
-				total += Character.getNumericValue(isbnNo.charAt(i)) * (10 -i);	
+				total += Character.getNumericValue(isbnNo.charAt(i)) * (SHORT_ISBN_NUMBER -i);	
 			}
 			
 		}
 		
-		if(total % 11 == 0) {
-			return true;
-		}else {
-			return false;
-		}
+		return (total % SHORT_ISBN_MULTIPLIER == 0);
 	}
 }
